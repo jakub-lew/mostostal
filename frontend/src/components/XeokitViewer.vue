f7ff2dd9f05d64fabc11ed0bfeb6a988060ea250
damianbobrowski <damian.bobrowski@creoox.com>
2024-10-05 17:10:16 +0200

    feat: deconstuction function

--- frontend/src/components/XeokitViewer.vue
+++ frontend/src/components/XeokitViewer.vue
@@ -3,6 +3,7 @@ import { Viewer, XKTLoaderPlugin } from '@xeokit/xeokit-sdk';
 import { onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { allowedModels } from '@/utils/config';
+import { storeToRefs } from 'pinia'
 
 const route = useRoute()
 const router = useRouter()
@@ -10,27 +11,27 @@ const fileName = route.params['name'] as string
 
 if (!allowedModels.includes(fileName)) {
   console.warn("File name not supported")
-  router.push({name: "home"})
+  router.push({ name: "home" })
 }
 
 onMounted(() => {
-    const viewer = new Viewer({
-        canvasId: "xeokit_canvas",
-        transparent: true,
-        dtxEnabled: true
-    });
+  const viewer = new Viewer({
+    canvasId: "xeokit_canvas",
+    transparent: true,
+    dtxEnabled: true
+  });
 
-    viewer.camera.eye = [-3.933, 2.855, 27.018];
-    viewer.camera.look = [4.400, 3.724, 8.899];
-    viewer.camera.up = [-0.018, 0.999, 0.039];
+  viewer.camera.eye = [-3.933, 2.855, 27.018];
+  viewer.camera.look = [4.400, 3.724, 8.899];
+  viewer.camera.up = [-0.018, 0.999, 0.039];
 
-    const xktLoader = new XKTLoaderPlugin(viewer);
+  const xktLoader = new XKTLoaderPlugin(viewer);
 
-    const sceneModel = xktLoader.load({
-        id: "myModel",
-        src: `http://127.0.0.1:5200/${fileName}.xkt`,
-        edges: true,
-    });
+  const sceneModel = xktLoader.load({
+    id: "myModel",
+    src: `http://127.0.0.1:5200/${fileName}.xkt`,
+    edges: true,
+  });
 
 });
 
@@ -38,14 +39,14 @@ onMounted(() => {
 </script>
 
 <template>
-        <canvas id="xeokit_canvas"></canvas>
+  <canvas id="xeokit_canvas"></canvas>
 </template>
 
 <style scoped>
 #xeokit_canvas {
-    height: 100%;
-    width: 100%;
-    background: lightblue;
-    background-image: linear-gradient(lightblue, white);
+  height: 100%;
+  width: 100%;
+  background: lightblue;
+  background-image: linear-gradient(lightblue, white);
 }
 </style>
